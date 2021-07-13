9e9a4f7f94bb6c7b7b52b3b98659032e700e234e
YunPC <apsn2000@nate.com>
2021-07-13 16:09:25 +0900

    feat: Make timer start feature 타이머에서 시작 버튼을 눌렀을 때 시간이 올라가는 기능 구현

--- src/js/stopwatch.js
+++ src/js/stopwatch.js
@@ -1,1 +1,22 @@
 const elapsedTime = {hh:0, mm:0, ss:0, ms:0};
+
+const timer = (()=> {
+    let timerId = null;
+    let {hh, mm, ss, ms} = elapsedTime;
+
+    timerId = setInterval(() => {
+        ms += 1;
+        if(ms >= 100){
+            ss += 1;
+            ms = 0;
+        }
+        if(ss >= 60){
+            mm += 1;
+            ss = 0;
+        }
+        if(mm >= 60){
+            hh += 1;
+            mm = 0;
+        }
+    })
+})();
