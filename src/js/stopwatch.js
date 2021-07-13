179e53e4289861928d4167a306883f844df46762
YunPC <apsn2000@nate.com>
2021-07-13 16:20:25 +0900

    feat: Reset timer 타이머의 리셋 기능 구현

--- src/js/stopwatch.js
+++ src/js/stopwatch.js
@@ -25,6 +25,9 @@ const timer = (()=> {
         stop(){
             clearInterval(timerId);
         },
+        reset(){
+            elapsedTime = {hh:0, mm:0, ss:0, ms:0};
+        }
     }
 
     
